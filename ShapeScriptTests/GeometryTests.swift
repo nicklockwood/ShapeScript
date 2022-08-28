6615ed1415575fd33edeba8025949f7381bdbccf
Nick Lockwood <nick@charcoaldesign.co.uk>
2022-08-28 21:26:06 +0100

    Add Geometry.bounds test

---
+++ ShapeScriptTests/GeometryTests.swift
@@ -0,0 +1,59 @@
+//
+//  GeometryTests.swift
+//  ShapeScriptTests
+//
+//  Created by Nick Lockwood on 28/08/2022.
+//  Copyright © 2022 Nick Lockwood. All rights reserved.
+//
+
+import Euclid
+@testable import ShapeScript
+import XCTest
+
+class GeometryTests: XCTestCase {
+    // MARK: Bounds
+
+    func testGroupOfShapeCameraBoundsNotEmpty() {
+        let cube = Geometry(
+            type: .cube,
+            name: nil,
+            transform: .identity,
+            material: .default,
+            smoothing: nil,
+            children: [],
+            sourceLocation: nil
+        )
+        let camera = Geometry(
+            type: .camera(Camera(
+                hasPosition: false,
+                hasOrientation: false,
+                hasScale: false
+            )),
+            name: nil,
+            transform: Transform(
+                offset: Vector(2.5539, 0.5531, 0.0131),
+                rotation: Rotation(
+                    roll: .radians(-0.5 * .pi),
+                    yaw: .radians(-0.4999 * .pi),
+                    pitch: .radians(-0.5 * .pi)
+                ),
+                scale: nil
+            ),
+            material: .default,
+            smoothing: nil,
+            children: [],
+            sourceLocation: nil
+        )
+        let group = Geometry(
+            type: .group,
+            name: nil,
+            transform: .identity,
+            material: .default,
+            smoothing: nil,
+            children: [cube, camera],
+            sourceLocation: nil
+        )
+        XCTAssert(camera.bounds.transformed(by: camera.transform).isEmpty)
+        XCTAssertEqual(group.bounds, cube.bounds)
+    }
+}
