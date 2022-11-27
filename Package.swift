5969caff0c00648b216708cc199ccf63a671bd80
Nick Lockwood <nick@charcoaldesign.co.uk>
2022-11-27 14:54:49 +0000

    Raise minimum Euclid version to 0.6.4

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/nicklockwood/Euclid.git",
-            .upToNextMajor(from: "0.5.30")
+            .upToNextMinor(from: "0.6.4")
         ),
         .package(
             url: "https://github.com/nicklockwood/LRUCache.git",
