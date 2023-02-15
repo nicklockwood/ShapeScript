8cffae6f0efd20471bd19d1ad82474968fa58240
Nick Lockwood <nick@charcoaldesign.co.uk>
2023-02-15 19:13:59 +0000

    Support multi-scene on iOS

--- Viewer/iOS/AppDelegate.swift
+++ Viewer/iOS/AppDelegate.swift
@@ -10,67 +10,14 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window?.backgroundColor = .black
-        return true
-    }
-
-    func application(_: UIApplication, open inputURL: URL, options _: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        var inputURL = inputURL
-        if inputURL.scheme == "shapescript" {
-            var path = inputURL.absoluteString
-                .deletingPrefix("shapescript:")
-                .deletingPrefix("//")
-
-            path = (path.removingPercentEncoding ?? path)
-                .deletingPrefix("file://")
-
-            if let url = URL(string: path), url.scheme ?? "file" != "file" {
-                inputURL = url
-            } else {
-                inputURL = URL(fileURLWithPath: path)
-            }
-        }
-
-        guard let documentBrowserViewController = window?.rootViewController as? DocumentBrowserViewController else { return false }
-
-        // Ensure the URL is a file URL
-        guard inputURL.isFileURL else {
-            documentBrowserViewController.presentError(
-                "Could not open '\(inputURL.absoluteString)'",
-                onOK: {}
-            )
-            return false
-        }
-
-        // Reveal / import the document at the URL
-        documentBrowserViewController.revealDocument(at: inputURL, importIfNeeded: true) { revealedDocumentURL, error in
-            if let error = error {
-                documentBrowserViewController.presentError(
-                    "Could not open file '\(inputURL.path)'. \(error.localizedDescription)",
-                    onOK: {}
-                )
-                return
-            }
-
-            // TODO: why is this needed?
-            let revealedDocumentURL = revealedDocumentURL ?? inputURL
-
-            // Present the Document View Controller for the revealed URL
-            documentBrowserViewController.presentDocument(at: revealedDocumentURL)
-        }
-
-        return true
-    }
-}
-
-private extension String {
-    func deletingPrefix(_ string: String) -> String {
-        if hasPrefix(string) {
-            return String(dropFirst(string.count))
-        }
-        return self
+    func application(
+        _: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options _: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        .init(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
     }
 }
---
+++ Viewer/iOS/SceneDelegate.swift
@@ -0,0 +1,81 @@
+//
+//  SceneDelegate.swift
+//  Viewer (iOS)
+//
+//  Created by Nick Lockwood on 15/02/2023.
+//  Copyright © 2023 Nick Lockwood. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo _: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        window?.backgroundColor = .black
+        
+        self.scene(scene, openURLContexts: connectionOptions.urlContexts)
+    }
+
+    func scene(_: UIScene, openURLContexts urlContexts: Set<UIOpenURLContext>) {
+        guard var inputURL = urlContexts.first?.url else {
+            return
+        }
+        if inputURL.scheme == "shapescript" {
+            var path = inputURL.absoluteString
+                .deletingPrefix("shapescript:")
+                .deletingPrefix("//")
+
+            path = (path.removingPercentEncoding ?? path)
+                .deletingPrefix("file://")
+
+            if let url = URL(string: path), url.scheme ?? "file" != "file" {
+                inputURL = url
+            } else {
+                inputURL = URL(fileURLWithPath: path)
+            }
+        }
+
+        guard let documentBrowserViewController = window?
+            .rootViewController as? DocumentBrowserViewController else { return }
+
+        // Ensure the URL is a file URL
+        guard inputURL.isFileURL else {
+            documentBrowserViewController.presentError(
+                "Could not open '\(inputURL.absoluteString)'",
+                onOK: {}
+            )
+            return
+        }
+
+        // Reveal / import the document at the URL
+        documentBrowserViewController.revealDocument(at: inputURL, importIfNeeded: true) { revealedDocumentURL, error in
+            if let error = error {
+                documentBrowserViewController.presentError(
+                    "Could not open file '\(inputURL.path)'. \(error.localizedDescription)",
+                    onOK: {}
+                )
+                return
+            }
+
+            // TODO: why is this needed?
+            let revealedDocumentURL = revealedDocumentURL ?? inputURL
+
+            // Present the Document View Controller for the revealed URL
+            documentBrowserViewController.presentDocument(at: revealedDocumentURL)
+        }
+    }
+}
+
+private extension String {
+    func deletingPrefix(_ string: String) -> String {
+        if hasPrefix(string) {
+            return String(dropFirst(string.count))
+        }
+        return self
+    }
+}
