73542ed2c4fbce3863fdd59ad39d390179176e54
Nick Lockwood <nick@charcoaldesign.co.uk>
2023-12-29 08:05:47 +0000

    Add binary STL export for CLI

--- Viewer/CLI/CLI+Export.swift
+++ Viewer/CLI/CLI+Export.swift
@@ -29,15 +29,23 @@ extension ExportOptions {
 }
 
 extension CLI {
-    static let exportTypes: [String] = ["stl"]
+    static let exportTypes: [String] = ["stl", "stla"]
 
     func export(_ geometry: Geometry, to url: URL, with options: ExportOptions) throws {
         var mesh = geometry.merged()
         if options.convertToZUp {
             mesh.rotate(by: .pitch(.degrees(-90)))
         }
-        let name = geometry.name ?? url.deletingPathExtension().lastPathComponent
-        let stl = mesh.stlString(name: name)
-        try stl.write(to: url, atomically: true, encoding: .utf8)
+        switch url.pathExtension.lowercased() {
+        case "stl":
+            let stl = mesh.stlData(colorLookup: { ($0 as? Material)?.color })
+            try stl.write(to: url, options: .atomic)
+        case "stla":
+            let name = geometry.name ?? url.deletingPathExtension().lastPathComponent
+            let stl = mesh.stlString(name: name)
+            try stl.write(to: url, atomically: true, encoding: .utf8)
+        case let ext:
+            throw CLIError("Unsupported export file type '\(ext)'")
+        }
     }
 }
